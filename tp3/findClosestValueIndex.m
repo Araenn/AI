610563d346be34bc02464edc1040c0143d5b671c
Léa Yeromonahos <leayero@gmail.com>
2023-11-04 18:57:36 +0100

    successfully finished ex1

--- tp3/findClosestValueIndex.m
+++ tp3/findClosestValueIndex.m
@@ -1,4 +1,8 @@
-function index = findClosestValueIndex(arr, targetValue)
-    differences = abs(arr - targetValue);
-    [~, index] = min(differences);
+function index = findClosestValueIndex(X, theta)
+    for i = 1:ceil(length(X))
+        x_point = X(i,1);
+        y_point = X(i,2);
+        distance(i) = abs((theta(1) + theta(2) * x_point + theta(3) * y_point) / sqrt(theta(2)^2 + theta(3)^2));
+    end
+    [~, index] = min(distance);
 end
--- tp3/svmq1.m
+++ tp3/svmq1.m
@@ -8,8 +8,8 @@ n = length(X);
 m = size(X, 2);
 
 c = zeros(n, 1);
-c(1:50) = 1;
-c(51:end) = -1;
+c(1:n/2) = 1;
+c(n/2+1:end) = -1;
 
 H = eye(m+1);
 H(1, 1) = 0;
@@ -29,27 +29,35 @@ x1 = (x1min:0.01:x1max)';
 
 x2 = -(theta(1) + theta(2) * x1)/ theta(3); %droite
 
-decision = w0 + X*w;
-index1 = findClosestValueIndex(decision, -1);
-index2 = findClosestValueIndex(decision, 1);
+%decision = w0 + X*w;
+index1 = findClosestValueIndex(X(1:n/2, :), theta);
+index2 = findClosestValueIndex(X(n/2+1:end, :), theta);
 
 figure(1)
-plot(X(1:50, 1), X(1:50, 2), 'x')
+plot(X(1:n/2, 1), X(1:n/2, 2), 'x')
 hold on
-plot(X(51:end, 1), X(51:end, 2), 'x')
+plot(X(n/2+1:end, 1), X(n/2+1:end, 2), 'x')
 plot(x1, x2)
-plot(X(index1), "s")
-plot(X(index2), "s")
+plot(X(index1, 1), X(index1, 2), "sb")
+plot(X(n/2+index2, 1), X(n/2+index2, 2), "sr")
 grid()
 
-
-[Xg,Yg] = meshgrid(x1,x2);
-f=theta(1)*Xg+theta(2)*Yg+theta(3);
+x2min=min(X(:,2));
+x2max=max(X(:,2));
+b2 = (x2min:0.01:x2max)';
+[Xg,Yg] = meshgrid(x1,b2);
+f= theta(2)*Xg + theta(3)*Yg + theta(1);
 fp=-ones(size(Xg));
 fp(f>=0)=1;
 
 figure(2)
-imagesc(x1,x2,fp);
+imagesc(x1,b2,fp);
 axis xy
-colormap('summer')
+colormap('sky')
 colorbar
+hold on
+plot(x1, x2, 'k', 'linewidth', 2)
+plot(X(1:n/2, 1), X(1:n/2, 2), 'xg')
+plot(X(n/2+1:end, 1), X(n/2+1:end, 2), 'xr')
+plot(X(index1, 1), X(index1, 2), "sk")
+plot(X(n/2+index2, 1), X(n/2+index2, 2), "sk")
